f082a932124c500b855aaae0cb0366c6dfe8a2a0
payal kutana <46380440+payalkutana@users.noreply.github.com>
2021-02-01 19:28:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'loggiing in python made easy - receive the log file you like in just one line of code!',   
   author = 'payal kutana',                   
   author_email = 'kutanapayal@gmail.com',    
-  url = 'https://github.com/payalkutana/logger/',  
+  url = 'https://github.com/payalkutana/logger',  
   download_url = 'https://github.com/payalkutana/logger/archive/v_01.tar.gz',   
   keywords = ['logger','logging','logs','easy'],   
   
