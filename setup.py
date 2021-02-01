0c2c75dc145d509de814a40a5819a7b17fb6df28
payal kutana <46380440+payalkutana@users.noreply.github.com>
2021-02-01 18:42:17 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'logger',         
+  packages = ['logger'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'this will write logs on file.',   
+  author = 'payal kutana',                   
+  author_email = 'kutanapayal@gmail.com',    
+  url = 'https://github.com/payalkutana/logger/',  
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',   
+  keywords = ['logger','logging','logs'],   
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
